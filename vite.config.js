da9286a7df0d7029a9286752bbef84e80b01799f
Sifa Mutiasya <sifamutiasya14@gmail.com>
2025-05-30 14:17:06 +0700

    commit for pwa

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,48 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+       devOptions: {
+        enabled: true
+      },
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'App FloodSight',
+        short_name: 'AppFloodSight',
+        description: 'PWA for FloodSight',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icons-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   css: {
     postcss: {
-      plugins: [
-        tailwindcss(),
-        autoprefixer(),
-      ],
+      plugins: [tailwindcss(), autoprefixer()],
     },
   },
   optimizeDeps: {
