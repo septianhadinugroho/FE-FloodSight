bf55c827f42187761e86d66a8df1a7a4e396bb3c
Sifa Mutiasya <sifamutiasya14@gmail.com>
2025-05-30 14:30:11 +0700

    commit to fix logo favicon.ico

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.l1v5mr30jpo"
+    "revision": "0.7gfvbqgte6g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
